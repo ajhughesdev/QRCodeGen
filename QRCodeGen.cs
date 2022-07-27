285cf0aca76479812ff159ccb3e2d8bf9c49e428
ajhughesdev <andrew@ajhughes.dev>
2022-07-27 17:09:08 -0400

    generate qr code to png

--- QRCodeGen.cs
+++ QRCodeGen.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Net.Codecrete.QrCodeGenerator;
 
 namespace QRCodeGen
 {
@@ -19,6 +20,11 @@ namespace QRCodeGen
       // get QR text from query string 
       string qrtext = req.Query["qrtext"];
       log.LogInformation("Generating QR Code for {0}", qrtext);
+
+      // create the QR Code from our text
+      var qr = QrCode.EncodeText(qrtext, QrCode.Ecc.Medium);
+      // convert it into a byte array for PNG output
+      var pngout = qr.ToPng(10, 1, SkiaSharp.SKColors.Black, SkiaSharp.SKColors.White);
     }
 
   }
