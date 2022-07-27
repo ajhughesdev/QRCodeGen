c4a447db09dbf6c46dd3bfa69daf23eb5e3e188f
ajhughesdev <andrew@ajhughes.dev>
2022-07-27 17:06:50 -0400

    create gr code generator function

--- QRCodeGen.cs
+++ QRCodeGen.cs
@@ -10,26 +10,14 @@ using Newtonsoft.Json;
 
 namespace QRCodeGen
 {
-    public static class QRCodeGen
+  public static class QRCodeGen
+  {
+    [FunctionName("GenerateQRCode")]
+    public static async Task<string> Generate(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
     {
-        [FunctionName("QRCodeGen")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
     }
+
+  }
 }
